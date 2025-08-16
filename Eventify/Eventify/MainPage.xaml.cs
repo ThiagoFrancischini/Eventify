b19476d8da74bfc24d0665505d1cfe00995c620c
Thiago Schmitz <thiagofrancischinischmitz@gmail.com>
2025-08-15 21:06:37 -0300

    Projeto blazor

---
+++ Eventify/Eventify/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Eventify
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Eventify/Eventify/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Eventify
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
