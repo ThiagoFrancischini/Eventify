415c7b71870aa0b1b2fb8356b3c6e253619a3376
Thiago Schmitz <thiagofrancischinischmitz@gmail.com>
2025-11-25 23:21:23 -0300

    Ajustes

--- Eventify/Eventify.Core/Interfaces/Repositories/IIngressoRepository.cs
+++ Eventify/Eventify.Core/Interfaces/Repositories/IIngressoRepository.cs
@@ -4,16 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Eventify.Core.Entities;
+using Eventify.Core.Filtros;
 
 namespace Eventify.Core.Interfaces.Repositories
 {
     public interface IIngressoRepository
     {
         Task<Ingresso?> GetById(Guid id);
+        Task<Ingresso?> GetByCodigo(string codigo);
         Task<List<Ingresso>> GetByEvent(Guid eventoId);
         Task<List<Ingresso>> GetByUser(Guid usuarioId);
         Task<List<Ingresso>> GetByCategory(Guid categoriaId);
-        Task<List<Ingresso>> GetAll();
+        Task<List<Ingresso>> GetAll(FiltroIngresso filtro);
         Task Salvar(Ingresso ingresso);
         Task Remover(Guid id);
     }
--- Eventify/Eventify.Infrastructure/Repositories/IngressoRepository.cs
+++ Eventify/Eventify.Infrastructure/Repositories/IngressoRepository.cs
@@ -1,4 +1,5 @@
 ﻿using Eventify.Core.Entities;
+using Eventify.Core.Filtros;
 using Eventify.Core.Interfaces.Repositories;
 using Eventify.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
@@ -23,13 +24,85 @@ namespace Eventify.Infrastructure.Repositories
                 .FirstOrDefaultAsync(i => i.Id == id);
         }
 
-        public async Task<List<Ingresso>> GetAll()
+        public async Task<Ingresso?> GetByCodigo(string codigo)
         {
             return await _context.Ingressos
                 .Include(i => i.Evento)
                 .Include(i => i.UsuarioCompra)
                 .Include(i => i.CategoriaIngresso)
-                .ToListAsync();
+                .FirstOrDefaultAsync(i => i.Codigo.ToUpper() == codigo.ToUpper());
+        }
+
+        public async Task<List<Ingresso>> GetAll(FiltroIngresso filtro)
+        {
+            var query = _context.Ingressos
+                .Include(i => i.Evento)
+                    .ThenInclude(e => e.Endereco)
+                .Include(i => i.UsuarioCompra)
+                .Include(i => i.CategoriaIngresso)
+                .AsQueryable();
+
+            if (filtro.EventoId.HasValue)
+            {
+                query = query.Where(x => x.EventoId == filtro.EventoId.Value);
+            }
+
+            if (filtro.UsuarioCompraId.HasValue)
+            {
+                query = query.Where(x => x.UsuarioCompraId == filtro.UsuarioCompraId.Value);
+            }
+
+            if (filtro.CategoriaIngressoId.HasValue)
+            {
+                query = query.Where(x => x.CategoriaIngressoId == filtro.CategoriaIngressoId.Value);
+            }
+
+            if (filtro.Valido.HasValue)
+            {
+                query = query.Where(x => x.Valido == filtro.Valido.Value);
+            }
+
+            if (filtro.DataCompraInicio.HasValue)
+            {
+                query = query.Where(x => x.DataCompra >= filtro.DataCompraInicio.Value);
+            }
+
+            if (filtro.DataCompraFim.HasValue)
+            {
+                query = query.Where(x => x.DataCompra <= filtro.DataCompraFim.Value);
+            }
+
+            if (!string.IsNullOrEmpty(filtro.Codigo))
+            {
+                query = query.Where(x => x.Codigo.Contains(filtro.Codigo));
+            }
+
+            if (filtro.Usado.HasValue)
+            {
+                if (filtro.Usado.Value)
+                {
+                    query = query.Where(x => x.DataUso != null);
+                }
+                else
+                {
+                    query = query.Where(x => x.DataUso == null);
+                }
+            }
+
+            if (filtro.OrderByDataCompraDesc)
+            {
+                query = query.OrderByDescending(x => x.DataCompra);
+            }
+            else if (filtro.OrderByDataCompra)
+            {
+                query = query.OrderBy(x => x.DataCompra);
+            }
+            else
+            {
+                query = query.OrderByDescending(x => x.DataCompra);
+            }
+
+            return await query.ToListAsync();
         }
 
         public async Task<List<Ingresso>> GetByEvent(Guid eventoId)
